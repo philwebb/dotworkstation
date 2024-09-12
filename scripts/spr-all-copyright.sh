24ffc150a5d4c8e2cdf9805d5ede7ce3702276d1
Phillip Webb <phil.webb@broadcom.com>
2024-09-11 19:37:50 -0700

    Update year

--- scripts/spr-all-copyright.sh
+++ scripts/spr-all-copyright.sh
@@ -1,1 +1,1 @@
-find . -type f -name "*.java" | xargs perl -pi -e "s/Copyright\ (\d{4})(-\d{4})?\ the\ original\ author\ or\ authors/Copyright \1-2023 the original author or authors/"
+find . -type f -name "*.java" | xargs perl -pi -e "s/Copyright\ (\d{4})(-\d{4})?\ the\ original\ author\ or\ authors/Copyright \1-2024 the original author or authors/"
