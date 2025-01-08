4aa123241539fe67c2e2f12dad4edfdd89363181
Phillip Webb <phil.webb@orbweaver.co.uk>
2025-01-08 11:24:00 -0800

    Update year

--- home/scripts/spr-year-to-date-copyright.sh
+++ home/scripts/spr-year-to-date-copyright.sh
@@ -1,1 +1,1 @@
-git --no-pager log --pretty=format: --name-only --since="2024-01-01" | sort | uniq | grep java | xargs perl -pi -e "s/Copyright\ (\d{4})(-\d{4})?\ the\ original\ author\ or\ authors/Copyright \1-2024 the original author or authors/"
+git --no-pager log --pretty=format: --name-only --since="2025-01-01" | sort | uniq | grep java | xargs perl -pi -e "s/Copyright\ (\d{4})(-\d{4})?\ the\ original\ author\ or\ authors/Copyright \1-2025 the original author or authors/"
