3966cbdda19f95a48b6fd5de89ab675dda36e863
Phillip Webb <phil.webb@orbweaver.co.uk>
2025-02-19 15:31:01 -0800

    Add secure scripts

---
+++ op/secscripts/spring-commercial.sh
@@ -0,0 +1,4 @@
+export COMMERCIAL_RELEASE_REPO_URL={{op://cli/spring-commercial/release-repo-url}}
+export COMMERCIAL_SNAPSHOT_REPO_URL={{op://cli/spring-commercial/snapshot-repo-url}}
+export COMMERCIAL_REPO_USERNAME={{op://cli/spring-commercial/username}}
+export COMMERCIAL_REPO_PASSWORD={{op://cli/spring-commercial/password}}
